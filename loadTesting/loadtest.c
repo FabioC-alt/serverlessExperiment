094941d0d25713d37d50953842ce71ee5566cf03
FabioC-alt <fabiociraci41@gmail.com>
2025-06-03 13:00:15 +0200

    Auto-commit on 2025-06-03 13:00:15

--- loadTesting/loadtest.c
+++ loadTesting/loadtest.c
@@ -4,8 +4,7 @@
 #include <string.h>       // strcmp
 #include <curl/curl.h>
 #include <time.h>
-
-#define INITIAL_SLEEP_MS 100   // initial delay in milliseconds
+#define INITIAL_SLEEP_MS 90000   // initial delay in milliseconds
 
 // Compute the difference in nanoseconds between two timespec values
 long long diff_ns(struct timespec start, struct timespec end) {
--- loadTesting/tests.sh
+++ loadTesting/tests.sh
@@ -1,12 +1,9 @@
 echo 'Inizio test hello'
 notify-send "Inizio Tests"
-./loadtest http://hello-fn.default.127.0.0.1.sslip.io const 100 latency_js_hello_endLatency_100_FabioPc.csv
-./loadtest http://hello-fn.default.127.0.0.1.sslip.io const 500 latency_js_hello_endLatencyInc_500_FabioPc.csv
 
-echo 'Terminato test hello'
-notify-send "Terminato test hello"
 
-echo 'Inizio test minio'
-./loadtest http://minio-node.default.127.0.0.1.sslip.io const 100 latency_js_minio_endLatency_100_FabioPc.csv
-./loadtest http://minio-node.default.127.0.0.1.sslip.io const 500 latency_js_minio_endLatencyInc_500_FabioPc.csv
-notify-send "Terminato test minio"
+./loadtest http://hello-fn.default.127.0.0.1.sslip.io const 50 latency_js_hello_coldStart_50_FabioPc.csv
+
+./loadtest http://minio-node.default.127.0.0.1.sslip.io  const 50 test.csv latency_js_hello_coldStart_50_FabioPc.csv
+
+notify-send "Fine test minio"

3a4a347e6dc0598ad383c37a3b2f4117e0a3be1b
FabioC-alt <fabiociraci41@gmail.com>
2025-06-03 14:23:49 +0200

    Auto-commit on 2025-06-03 14:23:49

--- loadTesting/tests.sh
+++ loadTesting/tests.sh
@@ -2,8 +2,6 @@ echo 'Inizio test hello'
 notify-send "Inizio Tests"
 
 
-./loadtest http://hello-fn.default.127.0.0.1.sslip.io const 50 latency_js_hello_coldStart_50_FabioPc.csv
-
-./loadtest http://minio-node.default.127.0.0.1.sslip.io  const 50 test.csv latency_js_hello_coldStart_50_FabioPc.csv
+./loadtest http://minio-node.default.127.0.0.1.sslip.io  const 50  latency_js_hello_coldStart_50_FabioPc.csv
 
 notify-send "Fine test minio"

56e70345749c8dc7148945966392e5a4368d3d57
FabioC-alt <fabiociraci41@gmail.com>
2025-06-05 12:00:22 +0200

    Auto-commit on 2025-06-05 12:00:22

--- loadTesting/tests.sh
+++ loadTesting/tests.sh
@@ -1,26 +1,10 @@
 notify-send "Inizio Tests hello Cold Start"
 
-./loadtestCS http://hello-quarkus.default.127.0.0.1.sslip.io const 50  latency_javaQuarkus_hello_coldStart_50_FabioPc.csv
-
-notify-send "Inizio Test minio Cold Start"
-
-./loadtestCS http://minio-quarkus.default.127.0.0.1.sslip.io const 50  latency_javaQuarkus_minio_coldStart_50_FabioPc.csv
-
-notify-send "Inizio Test hello End Latency"
-
-./loadtestEL http://hello-quarkus.default.127.0.0.1.sslip.io  const 500  latency_javaQuarkus_hello_endLatency_500_FabioPc.csv
+./loadtestEL http://minio-func.default.127.0.0.1.sslip.io  const 500  latency_go_hello_endLatency_500_FabioPc.csv
 
 notify-send "Inizio Test minio End Latency"
 
-./loadtestEL http://minio-quarkus.default.127.0.0.1.sslip.io const 500  latency_javaQuarkus_minio_endLatency_500_FabioPc.csv
-
-notify-send "Inizio Test hello End Latency Increm"
-
-./loadtestEL http://hello-quarkus.default.127.0.0.1.sslip.io  const 500  latency_javaQuarkus_hello_endLatencyInc_500_FabioPc.csv
-
-notify-send "Inizio Test minio End Latency Increm"
-
-./loadtestEL http://minio-quarkus.default.127.0.0.1.sslip.io  const 500  latency_javaQuarkus_minio_endLatencyInc_500_FabioPc.csv
+./loadtestEL http://minio-node.default.127.0.0.1.sslip.io  const 500  latency_node_hello_endLatency_500_FabioPc.csv
 
 notify-send "Fine test tutti i test"
 

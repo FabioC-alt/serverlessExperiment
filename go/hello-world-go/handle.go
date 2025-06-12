316b49d360701bce9a72d0c6d5362d082e45ec78
FabioC-alt <fabiociraci41@gmail.com>
2025-06-12 18:00:41 +0200

    Auto-commit on 2025-06-12 18:00:41

--- go/hello-world-go/handle.go
+++ go/hello-world-go/handle.go
@@ -1,26 +1,12 @@
 package function
 
 import (
-	"fmt"
-	"net/http"
-	"net/http/httputil"
+    "fmt"
+    "net/http"
 )
 
 // Handle an HTTP Request.
 func Handle(w http.ResponseWriter, r *http.Request) {
-	/*
-	 * YOUR CODE HERE
-	 *
-	 * Try running `go test`.  Add more test as you code in `handle_test.go`.
-	 */
-
-	dump, err := httputil.DumpRequest(r, true)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Println("Received request")
-	fmt.Printf("%q\n", dump)
-	fmt.Fprintf(w, "%q", dump)
+    fmt.Fprintln(w, "Hello, world!")
 }
+
